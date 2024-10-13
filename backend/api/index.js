344a66177a22b675a70554196241b8f47e8ff309
caesarc <46427941+caesarc6@users.noreply.github.com>
2024-10-13 15:59:03 -0400

    add api folder

---
+++ backend/api/index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+import { connectDB } from "../config/db.js";
+
+import entryRoutes from "../routes/entry.route.js";
+
+connectDB();
+
+const cors = require("cors");
+// Allow all origins
+// app.use(cors());
+// Allow specific origin(s)
+app.use(
+  cors({
+    origin: "https://gym-track-frontend.vercel.app/",
+  })
+);
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+app.use(express.json()); // allows to use json data in the body
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Server deployed and running on vercel.");
+});
+
+app.use("/api/entrys", entryRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} /* else if (process.env.NODE_ENV === "development") {
+//   app.use(express.static(path.join(__dirname, "/frontend/public")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
+//   });
+ } */
+
+// console.log(process.env.MONGO_URI);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server https://localhost:" + PORT);
+});
