9ef612c670d8e31cfe33b4f63579a05f315c31af
caesar <caesar.cuzco@jjay.cuny.edu>
2024-10-16 19:24:55 -0400

    change on api/index

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,27 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-
+import cors from "cors";
 import { connectDB } from "../config/db.js";
 
 import entryRoutes from "../routes/entry.route.js";
 
 connectDB();
 
-const cors = require("cors");
+dotenv.config();
 // Allow all origins
 // app.use(cors());
 // Allow specific origin(s)
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// connectDB();
+
 app.use(
   cors({
     origin: "https://gym-track-frontend.vercel.app/",
   })
 );
-
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 app.use(express.json()); // allows to use json data in the body
@@ -34,7 +35,7 @@ app.get("/", (req, res) => {
 app.use("/api/entrys", entryRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
@@ -48,6 +49,6 @@ if (process.env.NODE_ENV === "production") {
 // console.log(process.env.MONGO_URI);
 
 app.listen(PORT, () => {
-  connectDB();
+  // connectDB();
   console.log("Server https://localhost:" + PORT);
 });
