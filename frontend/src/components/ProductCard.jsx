21dbff88dab02fbf5360f707ce753ec1bfef7be6
caesarc <46427941+caesarc6@users.noreply.github.com>
2024-10-22 19:13:49 -0400

    added  date from function for title11

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -30,7 +30,7 @@ const ProductCard = ({ entry }) => {
   const [updatedEntry, setUpdatedEntry] = useState(entry);
   const [comment, setComment] = useState("");
   const textColor = useColorModeValue("gray.200", "gray.200");
-  const textColorOne = useColorModeValue("gray.600", "gray.100");
+  const textColorOne = useColorModeValue("gray.200", "gray.100");
   const bg = useColorModeValue("white", "gray.800");
   const { colorMode } = useColorMode();
   const { deleteEntry, updateEntry, likeEntry, commentEntry } =
@@ -208,7 +208,7 @@ const ProductCard = ({ entry }) => {
           {updatedEntry.name}
         </Heading>
         <Text
-          colorScheme="gray"
+          // colorScheme="gray"
           color={textColorOne}
           fontFamily="Arial, sans-serif"
         >
