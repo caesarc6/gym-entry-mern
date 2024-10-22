87a46f2fadef5642ae692e57b0bacb67b24e3852
caesarc <46427941+caesarc6@users.noreply.github.com>
2024-10-21 22:22:02 -0400

    added moment for dates and changed localhost to http instead of https in api/index

--- backend/api/index.js
+++ backend/api/index.js
@@ -51,6 +51,6 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(PORT, () => {
   // connectDB();
-  console.log("Server https://localhost:" + PORT);
+  console.log("Server http://localhost:" + PORT);
 });
 // module.exports = app;
