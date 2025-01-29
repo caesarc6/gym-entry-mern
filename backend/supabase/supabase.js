25708496684153214fdd6ea6525a99ad10eac79e
caesarc <46427941+caesarc6@users.noreply.github.com>
2025-01-29 01:33:00 -0500

    mod files

--- backend/supabase/supabase.js
+++ backend/supabase/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = "https://womzcoctdczisvsldmqd.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvbXpjb2N0ZGN6aXN2c2xkbXFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI1MDk2MzAsImV4cCI6MjA0ODA4NTYzMH0.DsOHipOdiyIY4oZG-IFQl2M3rrtiYV5tCkbkc8lxFz4";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
